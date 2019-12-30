8730d41e84147e27a1f885bbbf6b64e36057f06a
Celeste Lewis <cdlewis42@gmail.com>
2019-12-30 14:06:56 -0500

    Worked on Javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+
+var game = {
+    pokemonArray = ["Bulbasaur", "Abra", "Pikachu"]
+    
+    x=prompt("Please enter a letter")
+
+
+    comparison: function(x){
+    for (var i=0; i<this.pokemonArray.length[i];i++){
+        if x === pokemonArray[1][1]
+            console.log("pokemon is " + pokemonArray[i])
+            //show at bottom
+            //remove number of guesses
+            //show letter
+    }
+    }
+
+
+}
+console.log(game.pokemonArray[2])
+//prints Pikachu
+document.onkeyup = function{
+    (game.function())
+  }
